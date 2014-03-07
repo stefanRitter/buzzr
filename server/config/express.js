81f15c1af9781ad267f490ac034033ded4516d57
stefanRitter <stefan@stefanritter.com>
2014-03-07 19:36:54 +0100

    setup csrf

--- server/config/express.js
+++ server/config/express.js
@@ -21,6 +21,16 @@ module.exports = function (app, config) {
     
     app.use(express.session({secret: process.env.SESSION_SECRET || 'session secret'}));
     
+    // setup csrf for angular
+    app.use(express.csrf({value: function(req) {
+      return req.headers['x-xsrf-token'];
+    }}));
+    app.use(function(req, res, next) {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      next();
+    });
+
+
     app.use(passport.initialize());
     app.use(passport.session());
 
