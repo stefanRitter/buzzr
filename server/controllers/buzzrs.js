f0c25b26849a8d9aeee53bfdbc78cd14ffb0fe02
stefanRitter <stefan@stefanritter.com>
2014-03-09 10:09:49 +0100

    beta program

--- server/controllers/buzzrs.js
+++ server/controllers/buzzrs.js
@@ -7,7 +7,7 @@ exports.getByTopic = function (req, res) {
   var topic = decodeURI(req.params.id);
 
   Buzzr.findOne({topic: topic}).exec(function (err, obj) {
-    if (err) { return res.json({err: err); }
+    if (err) { return res.json({err: err}); }
     res.send(obj);
   });
 };
