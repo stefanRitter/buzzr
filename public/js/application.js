545b5ac3b095804d8152bd4e1c2416f68bf64d81
stefanRitter <stefan@stefanritter.com>
2014-03-16 00:37:09 +0100

    remove masonry

--- public/js/application.js
+++ public/js/application.js
@@ -241,34 +241,7 @@ angular.module('app').controller('appAdminUsersCtrl', function ($scope, AppUser)
   
   return isMobile;
 });
-;angular.module('app').directive('masonry', function($parse) {
-  return {
-    restrict: 'AC',
-    link: function(scope, elem, attrs) {
-      scope.items = [];
-      var container = elem[0];
-      var options = angular.extend({
-          itemSelector: '.item'
-      }, JSON.parse(attrs.masonry));
-
-      scope.obj = new Masonry(container, options);
-    }
-  };
-}).directive('masonryTile', function() {
-  return {
-    restrict: 'AC',
-    link: function(scope, elem) {
-      var master = elem.parent('*[masonry]:first').scope();
-      var masonry = master.obj;
-
-      elem.ready(function() {
-        masonry.addItems([elem]);
-        masonry.reloadItems();
-        masonry.layout();
-      });
-    }
-  };
-});;angular.module('app').factory('appNotifier', function () {
+;angular.module('app').factory('appNotifier', function () {
   return {
     notify: function (msg, scope) {
       scope.notifier = {};
