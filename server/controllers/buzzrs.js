7b85b7bf6697d24aa5cab3cc5a0f5261bca3f506
stefanRitter <stefan@stefanritter.com>
2014-03-23 09:25:10 +0000

    undo remove

--- server/controllers/buzzrs.js
+++ server/controllers/buzzrs.js
@@ -7,10 +7,6 @@ var Buzzr = require('mongoose').model('Buzzr'),
 exports.getByTopic = function (req, res) {
   var topic = decodeURI(req.params.id).toLowerCase().trim();
 
-  Buzzr.findOne({topic: 'giorgio armani'}, function(err, buzzr) {
-    buzzr.remove();
-  });
-
   Buzzr.findOne({topic: topic}).exec(function (err, buzzr) {
     if (err) { return res.json({err: err}); }
     
