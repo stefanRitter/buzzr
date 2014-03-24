b39d424a0dacf8c72e49f442a2ff39f7229b83bc
stefanRitter <stefan@stefanritter.com>
2014-03-24 13:18:41 +0000

    remove

--- server/controllers/buzzrs.js
+++ server/controllers/buzzrs.js
@@ -3,18 +3,8 @@
 var Buzzr = require('mongoose').model('Buzzr');
 var buzzrCreator = require('child_process').fork('server/utils/buzzrCreator.js');
 
-
-function remove(err, buzzr) {
-  buzzr.remove();
-}
-  
 exports.getByTopic = function (req, res) {
   var topic = decodeURI(req.params.id).toLowerCase().trim();
-
-  Buzzr.findOne({topic: 'mountainview'}, remove);
-  Buzzr.findOne({topic: 'faberge eggs'}, remove);
-  Buzzr.findOne({topic: 'faberge egg'}, remove);
-  Buzzr.findOne({topic: 'british museum'}, remove);
 
   Buzzr.findOne({topic: topic}).exec(function (err, buzzr) {
     if (err) { return res.json({err: err}); }
