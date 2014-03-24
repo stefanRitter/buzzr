0fa1a8917549830c0c66306213b03aa351644968
stefanRitter <stefan@stefanritter.com>
2014-03-24 12:34:28 +0000

    slice

--- server/controllers/buzzrs.js
+++ server/controllers/buzzrs.js
@@ -19,7 +19,7 @@ exports.getByTopic = function (req, res) {
         len = links.length;
 
     if (len > 120) {
-      return res.send({links: links.reverse().slice(0,120)});
+      //return res.send({links: links.reverse().slice(0,120)});
     }
     res.send({links: links});
   });
