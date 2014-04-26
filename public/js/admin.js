98d1dc1ee4060824c91926b00083f0f23de5bdba
stefanRitter <stefan@stefanritter.com>
2014-04-26 08:03:24 +0100

    serve fonts

--- public/js/admin.js
+++ public/js/admin.js
@@ -411,6 +411,10 @@ angular.module('app').run(function ($rootScope, $location) {
     $scope.toggle();
   });
 });
+;angular.module('app').controller('appHeaderCtrl', function ($scope) {
+  'use strict';
+  $scope.header = {};
+});
 ;angular.module('app').factory('appBuzzr', function ($http, $route, appProcessLinks) {
   'use strict';
   var BuzzrResource = {};
