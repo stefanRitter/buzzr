9722acd75100839ad6fc2483d29ab89efb811d67
stefanRitter <stefan@stefanritter.com>
2014-04-27 11:21:08 +0100

    clean emptys error arrays

--- worker/clean/index.js
+++ worker/clean/index.js
@@ -1,14 +1,22 @@
 'use strict';
 
-var Buzzr = require('mongoose').model('Buzzr');
+var Buzzr = require('mongoose').model('Buzzr'),
+    LinkArray = require('mongoose').model('LinkArray');
 
 Buzzr.find({}, function(err, buzzrs) {
   if (err) { throw err; }
 
   buzzrs.forEach(function(buzzr) {
     console.log('CLEAN: ' + buzzr.topic);
-    //buzzr.makeUniq();
+    buzzr.makeUniq();
     buzzr.twitPoints.sinceId = '0';
     buzzr.save();
+  });
+});
+
+LinkArray.find({}).exec(function(err, linkArrays) {
+  linkArrays.forEach(function(arr) {
+    arr.array = [];
+    arr.save();
   });
 });
