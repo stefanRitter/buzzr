978e2410f1a3e615d69fa6e3a41e9a61383ea11c
stefanRitter <stefan@stefanritter.com>
2014-04-28 16:31:12 +0100

    only show 10 retweets

--- worker/common/calcTweetRank.js
+++ worker/common/calcTweetRank.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = function(tweet) {
-  var favs = tweet.favorite_count || 0,
+  var favs = 0, //tweet.favorite_count || 0,
       retweets = tweet.retweet_count || 0,
       rank = favs + retweets;
 
   if (!!tweet.in_reply_to_status_id) {
-    rank += 1;
+    rank += 10;
   }
   return rank;
 };
