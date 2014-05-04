ae1f77b9c2299949874858ec411b60423fd02584
stefanRitter <stefan@stefanritter.com>
2014-05-04 11:26:03 +0100

    add server status

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,15 +1,17 @@
 'use strict';
 
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var sendgrid  = require('sendgrid')(
   process.env.SENDGRID_USERNAME,
   process.env.SENDGRID_PASSWORD
 );
 
+
 exports.send = function(text) {
   sendgrid.send({
     to: ['stefan@stefanritter.com', 'jeroen.h.s.roosen@gmail.com'],
     from: 'server@buzzr.io',
-    subject: 'BUZZR SERVER MESSAGE',
+    subject: 'BUZZR SERVER MESSAGE from ' + env,
     text: text
   }, function(err, json) {
     if (err) { return console.error(err); }
