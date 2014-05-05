f5b81085da1dffdeef58886961551bbbc9b16337
stefanRitter <stefan@stefanritter.com>
2014-05-05 19:06:24 +0100

    impl controller

--- server/controllers/tweet4me.js
+++ server/controllers/tweet4me.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var Tweet4me = require('mongoose').model('Tweet4me');
+
 exports.getByUser = function(req, res) {
-  res.json({tweets: []});
+  var user = req.params.id.toLowerCase().trim();
+
+  Tweet4me.findOne({user: user}).exec(function(err, tweet4me) {
+    if (err) { return res.send(500); }
+    if (!tweet4me) { return res.json({tweets: []}); }
+
+    res.json({tweets: tweet4me.tweets});
+  });
 };
