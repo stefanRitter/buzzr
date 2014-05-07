e6dca9597340f8e90f9e2ba9c740e0ee2434e0b3
stefanRitter <stefan@stefanritter.com>
2014-05-07 20:29:22 +0100

    reset

--- server/models/Tweet4me.js
+++ server/models/Tweet4me.js
@@ -42,9 +42,9 @@ tweet4meSchema.methods.isDublicate = function(url) {
 Tweet4me = mongoose.model('Tweet4me', tweet4meSchema);
 
 Tweet4me.find({}).exec(function(err, collection) {
-  /*collection.forEach(function(model) {
+  collection.forEach(function(model) {
     model.remove();
-  });*/
+  });
   if (collection.length === 0) {
     Tweet4me.create({
       user: 'jenbolan@icloud.com',
@@ -53,17 +53,17 @@ Tweet4me.find({}).exec(function(err, collection) {
         topic: 'aloe vera',
         url: 'http://wp.me/pE94w-2ih',
         text: 'Benefits of Aloe Vera Juice http://wp.me/pE94w-2ih via @wordpressdotcom',
-        added: new Date(2014, 5, 7)
+        added: new Date('May 7, 2014')
       }, {
         topic: 'work from home',
         url: 'http://www.buzzfeed.com/adamdavis/thoughts-you-always-have-when-you-work-from-home',
         text: '71 Thoughts You Have When You Work From Home http://www.buzzfeed.com/adamdavis/thoughts-you-always-have-when-you-work-from-home via @amdhit @buzzfeed',
-        added: new Date(2014, 5, 7)
+        added: new Date('May 7, 2014')
       }, {
         topic: 'living the dream',
         url: 'http://www.projecteve.com/living-the-dream/',
         text: 'Living the dream http://www.projecteve.com/living-the-dream/ via @projecteve1',
-        added: new Date(2014, 5, 7)
+        added: new Date('May 7, 2014')
       }]
     });
 
@@ -74,18 +74,18 @@ Tweet4me.find({}).exec(function(err, collection) {
         topic: 'skateboarding',
         url: 'http://skateboarding.transworld.net/1000197156/videos/skate-cause-video/',
         text: 'Skate For A Cause Video - Transworld Skateboarding: http://skateboarding.transworld.net/1000197156/videos/skate-cause-video/',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'sports',
         url: 'http://sports.yahoo.com/news/world-cup-security-compared-iraq-afghanistan-184701292--sow.html',
         text: 'World Cup security compared with Iraq, Afghanistan http://sports.yahoo.com/news/world-cup-security-compared-iraq-afghanistan-184701292--sow.html via @YahooSports',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'skateboarding',
         url: 'http://www.theguardian.com/film/video/2014/may/02/hill-street-dublin-skateboarding-documentary-video',
         text: 'Hill Street: How Dublin ground out its place in the skateboarding scene - video ' +
               'http://www.theguardian.com/film/video/2014/may/02/hill-street-dublin-skateboarding-documentary-video via @guardian',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }]
     });
     
@@ -96,17 +96,17 @@ Tweet4me.find({}).exec(function(err, collection) {
         topic: 'minecraft',
         url: 'http://buswk.co/1san1xb',
         text: 'To Teach and Delight, Denmark Recreates Itself in Minecraft  http://buswk.co/1san1xb via @BW',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'minecraft',
         url: 'http://l.gamespot.com/1fIBBKL',
         text: 'This is why we can\'t have nice things: Players destroy Danish government\'s Minecraft model. http://l.gamespot.com/1fIBBKL',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'minecraft',
         url: 'http://aol.it/1mwqq64',
         text: 'Captive Minecraft world puts baby in a corner  http://aol.it/1mwqq64 via @joystiq',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }]
     });
 
@@ -117,34 +117,34 @@ Tweet4me.find({}).exec(function(err, collection) {
         topic: 'graphic design',
         url: 'http://shar.es/SGuTI',
         text: '10 best graphic design tutorials & tips - Features http://shar.es/SGuTI via @digital_arts',
-        added: new Date(2014, 5, 5)
+        added: new Date('May 5, 2014')
       }, {
         topic: 'SEO',
         url: 'http://goo.gl/9SrFpd',
         text: '10 SEO Mistakes You Can\'t Afford with Your Content http://goo.gl/9SrFpd  via @Gazalla',
-        added: new Date(2014, 5, 5)
+        added: new Date('May 5, 2014')
       }, {
         topic: 'photography',
         url: 'http://www.tocofi.com/is-there-a-wrong-way-to-hold-a-camera/',
         text: 'Is There a Wrong Way to Hold a Camera? http://www.tocofi.com/is-there-a-wrong-way-to-hold-a-camera/',
-        added: new Date(2014, 5, 5)
+        added: new Date('May 5, 2014')
       },
 
       {
         topic: 'photography',
         url: 'http://www.instructables.com/id/Double-exposure-photography-with-an-iphone/',
         text: '6 steps to Double exposure photography with an iphone http://www.instructables.com/id/Double-exposure-photography-with-an-iphone/',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'photography',
         url: 'http://www.digitalcameraworld.com/2014/04/30/camera-cleaning-5-ways-to-healthcheck-your-camera-with-confidence/',
         text: 'Camera cleaning: 5 ways to healthcheck your camera with confidence! http://www.digitalcameraworld.com/2014/04/30/camera-cleaning-5-ways-to-healthcheck-your-camera-with-confidence/',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'graphic design',
         url: 'http://www.creativebloq.com/typography/pro-tips-flawless-typography-5132670',
         text: '10 pro tips for perfect typography @CreativeBloQ http://www.creativebloq.com/typography/pro-tips-flawless-typography-5132670',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }]
     });
 
@@ -155,17 +155,17 @@ Tweet4me.find({}).exec(function(err, collection) {
         topic: 'running',
         url: 'http://po.st/zQPYnh',
         text: 'Workout Of The Week: Tempo Run—With A Twist! - Competitor.com http://po.st/zQPYnh via @runcompetitor',
-        added: new Date(2014, 5, 5)
+        added: new Date('May 5, 2014')
       }, {
         topic: 'running',
         url: 'http://www.theguardian.com/lifeandstyle/the-running-blog/2014/may/05/weekend-running-post-run-workout-treats-food',
         text: 'How was your weekend running? http://www.theguardian.com/lifeandstyle/the-running-blog/2014/may/05/weekend-running-post-run-workout-treats-food via @guardian',
-        added: new Date(2014, 5, 5)
+        added: new Date('May 5, 2014')
       }, {
         topic: 'running',
         url: 'https://www.youtube.com/watch?v=4WVTG3dXuVM',
         text: 'Need #motivation to go running? Check out these Geese jogging down the road https://www.youtube.com/watch?v=4WVTG3dXuVM',
-        added: new Date(2014, 5, 5)
+        added: new Date('May 5, 2014')
       },
 
       {
@@ -173,17 +173,34 @@ Tweet4me.find({}).exec(function(err, collection) {
         url: 'http://www.express.co.uk/life-style/health/474180/Reviews-of-the-latest-trainers-and-running-shoes-from-Adidas-Nike-Puma-and-New-Balance',
         text: 'Reviews of the latest trainers and running shoes from Adidas, Nike, Puma and New Balance '+
               'http://www.express.co.uk/life-style/health/474180/Reviews-of-the-latest-trainers-and-running-shoes-from-Adidas-Nike-Puma-and-New-Balance via @daily_express',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'running',
         url: 'http://lissyruns.com/2014/05/06/why-running-a-marathon-isnt-the-hardest-thing-youll-ever-do/',
         text: 'Why running a marathon isn’t the hardest thing you’ll ever do http://lissyruns.com/2014/05/06/why-running-a-marathon-isnt-the-hardest-thing-youll-ever-do/ via @lissyruns',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
       }, {
         topic: 'running',
         url: 'http://po.st/8MZDi4',
         text: 'Video: Sh*t Barefoot Runners Say http://po.st/8MZDi4 via @runcompetitor',
-        added: new Date(2014, 5, 6)
+        added: new Date('May 6, 2014')
+      },
+
+      {
+        topic: 'running',
+        url: 'http://www.runnersworld.com/injury-prevention-recovery/the-importance-of-breaking-in-new-shoes',
+        text: 'The Importance of Breaking In New Shoes via @RunnersWorld http://www.runnersworld.com/injury-prevention-recovery/the-importance-of-breaking-in-new-shoes',
+        added: new Date('May 7, 2014')
+      }, {
+        topic: 'running',
+        url: 'http://www.active.com/fitness/articles/why-is-a-strong-core-important',
+        text: 'Why is a Strong Core Important for #running? http://www.active.com/fitness/articles/why-is-a-strong-core-important via @active',
+        added: new Date('May 7, 2014')
+      }, {
+        topic: 'running',
+        url: 'http://blog.runkeeper.com/1657/why-you-need-a-strong-core-for-running/',
+        text: 'Great tips for increasing core strength and your running stability: http://blog.runkeeper.com/1657/why-you-need-a-strong-core-for-running/ via @runkeeper',
+        added: new Date('May 7, 2014')
       }]
     });
   }
