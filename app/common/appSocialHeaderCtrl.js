489f92ddeab580a047226e73ceb62e1fd5d7c0d8
stefanRitter <stefan@stefanritter.com>
2014-05-10 10:11:55 +0100

    5 day free

--- app/common/appSocialHeaderCtrl.js
+++ app/common/appSocialHeaderCtrl.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('appSocialHeaderCtrl', function ($scope, $location) {
   'use strict';
 
-  var show = ['/', '/tweet4me', '/tweet4me/pricing', '/terms'];
+  var show = ['/', '/tweet4me', '/tweet4me/pricing', '/tweet4me/join', '/terms'];
 
   $scope.showSocialHeader = function() {
     return show.indexOf($location.path()) > -1;
--- public/js/admin.js
+++ public/js/admin.js
@@ -696,7 +696,7 @@ angular.module('app').factory('appNotifier', function() {
 angular.module('app').controller('appSocialHeaderCtrl', function ($scope, $location) {
   'use strict';
 
-  var show = ['/', '/tweet4me', '/tweet4me/pricing', '/terms'];
+  var show = ['/', '/tweet4me', '/tweet4me/pricing', '/tweet4me/join', '/terms'];
 
   $scope.showSocialHeader = function() {
     return show.indexOf($location.path()) > -1;
