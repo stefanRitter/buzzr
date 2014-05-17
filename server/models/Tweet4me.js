cd69098249d69698963e0aa3a2db03581ce945c6
stefanRitter <stefan@stefanritter.com>
2014-05-17 10:02:34 +0100

    rm

--- server/models/Tweet4me.js
+++ server/models/Tweet4me.js
@@ -42,11 +42,11 @@ tweet4meSchema.methods.isDublicate = function(url) {
 Tweet4me = mongoose.model('Tweet4me', tweet4meSchema);
 
 Tweet4me.find({}).exec(function(err, collection) {
-  collection.forEach(function(t4m) {
+  /*collection.forEach(function(t4m) {
     if (t4m.user === 'pee@poopmail.com') {
       t4m.remove();
     }
-  });
+  });*/
 
   if (collection.length === 0) {
     Tweet4me.create({
