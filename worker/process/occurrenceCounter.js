71e44fdfe86c3f6fb682525b6ef01819770d6455
stefanRitter <stefan@stefanritter.com>
2014-05-28 19:56:45 +0100

    fix occurence counter

--- worker/process/occurrenceCounter.js
+++ worker/process/occurrenceCounter.js
@@ -1,22 +1,22 @@
 'use strict';
 
-module.exports = function () { //(link, topic) {
-  /*var searchHead = link.headText.search(topic),
-      searchBody = link.bodyText.search(topic),
-      found = searchHead !== -1 && searchBody !== -1;
+module.exports = function(link, topic) {
+  if (!link.bodyText) { return false; }
+
+  var searchBody = link.bodyText.search(topic),
+      found = searchBody > -1;
 
   if (!found) {
     var words = topic.split(' '),
         wordsFound = [];
 
     words.forEach(function(word, i) {
-      searchHead = link.headText.search(word);
       searchBody = link.bodyText.search(word);
-      wordsFound[i] = searchHead !== -1 && searchBody !== -1;
+      wordsFound[i] = searchBody > -1;
     });
 
     found = wordsFound.indexOf(false) === -1;
   }
-  //return found;*/
-  return true;
+  
+  return found;
 };
