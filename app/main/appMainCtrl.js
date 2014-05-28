e500d3607f1dee8471a5386c325716880c384756
stefanRitter <stefan@stefanritter.com>
2014-05-28 22:16:17 +0100

    add count down

--- app/main/appMainCtrl.js
+++ app/main/appMainCtrl.js
@@ -2,7 +2,7 @@ angular.module('app').controller('appMainCtrl', function ($scope, $routeParams, 
   /*jshint maxstatements: false */
   'use strict';
 
-  $scope.countDown = 30;
+  $scope.countDown = 60;
   $scope.links = [];
   $scope.dates = [];
   $scope.lang = '';
