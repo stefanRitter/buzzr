22b98f7bba2185dbdeda365324f71ecd8dfc66c0
stefanRitter <stefan@stefanritter.com>
2014-05-30 21:48:44 +0100

    remove t4m

--- public/js/admin.js
+++ public/js/admin.js
@@ -286,6 +286,48 @@ angular.module('app').controller('appUnsubscribeCtrl', function ($scope) {
 
   $scope.done = true;
 });
+angular.module('app').controller('appTweet4meUpgradeCtrl', function ($scope, $http) {
+  'use strict';
+  var stripeToken = {};
+  var planAmounts = {
+      'startup': 10*100,
+      'business': 49*100,
+      'enterprise': 99*100
+    };
+
+  $scope.success = false;
+  $scope.plan = '';
+  
+  var handler = window.StripeCheckout.configure({
+    key: 'pk_live_xGJ0UWxpKbFmhRVaXUcMDuIG', //'pk_test_A92gZzXMijuUIYouO3UXkIyB'
+    image: '/img/icon.png',
+    token: function(token) {
+      stripeToken = token;
+
+      $http
+        .post('/stripe', {token: token, plan: $scope.plan})
+        .then(function(res) {
+          if (res.data.success) {
+            $scope.success = true;
+            $scope.email = token.email;
+          } else {
+            window.alert(res.data.reason);
+          }
+        }, function() {
+          window.alert('There was a server error, your card was NOT charged! Please contact us for help!');
+        });
+    }
+  });
+
+  $scope.openCheckout = function(plan) {
+    $scope.plan = plan;
+    handler.open({
+      name: 'Buzzr',
+      description: 'Upgrade: ' + plan,
+      amount: planAmounts[plan]
+    });
+  };
+});
 angular.module('app').controller('appAdminBuzzrsCtrl', function ($scope, $http, $window) {
   'use strict';
   
