1af83a99ee9ba42da134c48cd5f2ae9b741fb474
stefanRitter <stefan@stefanritter.com>
2014-05-31 08:36:59 +0100

    update cronjob

--- server/utils/cronjob.js
+++ server/utils/cronjob.js
@@ -2,37 +2,33 @@
 
 var sendMail = require('./sendMail.js'),
     Buzzr = require('mongoose').model('Buzzr'),
-    fork = require('child_process').fork;
+    fork = require('child_process').fork,
+    lastRun = 0;
 
-var running = false;
 
 function run() {
-  running = true;
-
   Buzzr.find({}).exec(function(err, collection) {
     if (err) {
       sendMail.send('agenda error: ' + err.toString());
       throw err;
     }
 
-    var l = collection.length;
     collection.forEach(function(buzzr, i) {
       setTimeout(function() {
         fork('server/utils/buzzrMaker.js').send({topic: buzzr.topic});
-        
-        if (i >= l-1) {
-          sendMail.send('agenda finished!');
-          running = false;
-        }
       }, 60000*i);
     });
   });
 }
 
 setInterval(function() {
-  var h = (new Date()).getHours();
-  if (h >= 4 && h <= 5 && !running) {
+  var d = new Date(),
+      h = d.getHours(),
+      t = d.getUTCDate();
+  
+  if (h >= 9 && h <= 10 && t !== lastRun) {
     sendMail.send('agenda started');
+    lastRun = t;
     run();
   }
 }, 60000*40);
