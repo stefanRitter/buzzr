6ce1da82093ead25a3e6af11b89388759e7b90f7
stefanRitter <stefan@stefanritter.com>
2014-06-01 08:49:43 +0100

    bring back old occ counter

--- worker/process/occurrenceCounter.js
+++ worker/process/occurrenceCounter.js
@@ -1,23 +1,23 @@
 'use strict';
 
 module.exports = function() {return true;};
-/*function(link, topic) {
-  if (!link.bodyText) { return false; }
-
-  var searchBody = link.bodyText.search(topic),
-      found = searchBody > -1;
+/*function (link, topic) {
+  var searchHead = link.headText.search(topic),
+      searchBody = link.bodyText.search(topic),
+      found = searchHead !== -1 && searchBody !== -1;
 
   if (!found) {
     var words = topic.split(' '),
         wordsFound = [];
 
     words.forEach(function(word, i) {
+      searchHead = link.headText.search(word);
       searchBody = link.bodyText.search(word);
-      wordsFound[i] = searchBody > -1;
+      wordsFound[i] = searchHead !== -1 && searchBody !== -1;
     });
 
     found = wordsFound.indexOf(false) === -1;
   }
-  
-  return found;
+  //return found;
+  return true;
 };*/
