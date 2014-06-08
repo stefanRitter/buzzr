09b0b8831c2b3ce94a855b88b62e0b19e0263d85
stefanRitter <stefan@stefanritter.com>
2014-06-08 08:00:59 +0100

    timeout

--- server.js
+++ server.js
@@ -3,27 +3,30 @@
 var dotenv = require('dotenv');
 dotenv.load();
 
-var express = require('express');
+function start() {
+  var express = require('express');
 
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-    config = require('./server/config/config')[env],
-    app = express();
+  var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
+      config = require('./server/config/config')[env],
+      app = express();
 
-console.log(config);
-console.log(process.env);
+  console.log(config);
 
-// setup datastore
-require('./server/config/mongoose.js')(config);
+  // setup datastore
+  require('./server/config/mongoose.js')(config);
 
-// setup express
-require('./server/config/express.js')(app, config);
+  // setup express
+  require('./server/config/express.js')(app, config);
 
-// setup auth
-require('./server/config/passport.js')();
+  // setup auth
+  require('./server/config/passport.js')();
 
-// setup routes
-require('./server/config/routes.js')(app);
+  // setup routes
+  require('./server/config/routes.js')(app);
 
 
-app.listen(config.port);
-console.log('listening on port ' + config.port);
+  app.listen(config.port);
+  console.log('listening on port ' + config.port);
+}
+
+setTimeout(start, 2000);
