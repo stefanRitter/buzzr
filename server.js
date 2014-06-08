af9e8ded678bab29045a6048e69b001d897f0103
stefanRitter <stefan@stefanritter.com>
2014-06-08 08:07:43 +0100

    remove timeout

--- server.js
+++ server.js
@@ -3,30 +3,26 @@
 var env = require('node-env-file');
 env('.env');
 
-function start() {
-  var express = require('express');
+var express = require('express');
 
-  var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-      config = require('./server/config/config')[env],
-      app = express();
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
+    config = require('./server/config/config')[env],
+    app = express();
 
-  console.log(process.env);
+console.log(process.env);
 
-  // setup datastore
-  require('./server/config/mongoose.js')(config);
+// setup datastore
+require('./server/config/mongoose.js')(config);
 
-  // setup express
-  require('./server/config/express.js')(app, config);
+// setup express
+require('./server/config/express.js')(app, config);
 
-  // setup auth
-  require('./server/config/passport.js')();
+// setup auth
+require('./server/config/passport.js')();
 
-  // setup routes
-  require('./server/config/routes.js')(app);
+// setup routes
+require('./server/config/routes.js')(app);
 
 
-  app.listen(config.port);
-  console.log('listening on port ' + config.port);
-}
-
-setTimeout(start, 2000);
+app.listen(config.port);
+console.log('listening on port ' + config.port);
