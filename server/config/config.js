e8d0675d8726616c2b74c9fe5fb1015eb6953a68
stefanRitter <stefan@stefanritter.com>
2014-07-01 08:06:00 +0100

    remove charge

--- server/config/config.js
+++ server/config/config.js
@@ -7,16 +7,12 @@ module.exports = {
   development: {
     datastoreURI: 'mongodb://localhost/buzzr',
     rootPath: rootPath,
-    port: 3030,
-    stripeSecret: 'sk_test_Ndya4eXFlDlm1ySefxlwjG4v',
-    stripeKey: 'pk_test_A92gZzXMijuUIYouO3UXkIyB'
+    port: 3030
   },
   
   production: {
     datastoreURI: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL,
     rootPath: rootPath,
-    port: process.env.PORT || 3030,
-    stripeSecret: 'sk_live_OJInktE9Nc0sjoSfNddg10De',
-    stripeKey: 'pk_live_xGJ0UWxpKbFmhRVaXUcMDuIG'
+    port: process.env.PORT || 3030
   }
 };
